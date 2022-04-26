75d8f25c0bb3448b3b2861fdb0eb3e00cf86a90b
irving jacome <irving2291@gmail.com>
2022-04-25 20:36:27 -0500

    icons

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get_it/get_it.dart';
 import 'package:pet_card/data/authentication_client.dart';
 import 'package:pet_card/pages/login_page.dart';
 import 'package:pet_card/utils/my_colors.dart';
+import 'package:pet_card/utils/pets_icons.dart';
 import 'package:pet_card/utils/responsive.dart';
 
 class HomePage extends StatefulWidget {
@@ -24,7 +28,7 @@ class _HomePageState extends State<HomePage> {
     final _items = <Widget>[
       const Icon(Icons.home, size: 30,),
       const Icon(Icons.search, size: 30,),
-      const Icon(Icons.favorite, size: 30, color: Colors.transparent,),
+      const Icon(PetsIcons.truck, size: 35,),
       const Icon(Icons.settings, size: 30,),
       const Icon(Icons.person, size: 30,),
     ];
@@ -33,34 +37,57 @@ class _HomePageState extends State<HomePage> {
 
     return Scaffold(
       backgroundColor: Colors.white70,
+      drawer: Drawer(
+        child: Column(
+          children: [
+            const DrawerHeader(child: Text("I am"),),
+            TextButton(
+              onPressed: () async {
+                await _authenticationClient.signOut();
+                Navigator.pushNamedAndRemoveUntil(
+                  context,
+                  LoginPage.routeName, (route) => false,
+                );
+              },
+              child: const Text("Logout"),
+            ),
+          ],
+        ),
+      ),
       body: GestureDetector(
         onTap: () {
           FocusScope.of(context).unfocus();
         },
-        child: Center(
-          child: TextButton(
-            onPressed: () async {
-              await _authenticationClient.signOut();
-              Navigator.pushNamedAndRemoveUntil(
-                context,
-                LoginPage.routeName, (route) => false,
-              );
-            },
-              child: const Text("Logout"),
-          ),
+        child: SingleChildScrollView(
+          child: Container(
+            width: double.infinity,
+            height: responsive.height,
+            child: Row(
+              children: const [
+                Card(
+                  child: Text("Age"),
+                )
+              ],
+            ),
+          )
         ),
       ),
       bottomNavigationBar: Theme(
         data: Theme.of(context).copyWith(
-          iconTheme: IconThemeData(color: Colors.white),
+          iconTheme: const IconThemeData(color: MyColors.purpleOne),
         ),
         child: CurvedNavigationBar(
           buttonBackgroundColor: Colors.white,
           backgroundColor: Colors.transparent,
           color: MyColors.ripeOrange,
-          height: 52,
+          height: Platform.isAndroid ? 52 : 75,
           items: _items,
           index: _indexNavigatorBar,
+          onTap: (value) {
+            setState(() {
+              _indexNavigatorBar = value;
+            });
+          },
         ),
       ),
     );
